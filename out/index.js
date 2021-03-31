a20fddaebe0fa67d3e1567f21fc47567b750bd81
LockdownPT <guerra70@live.com.pt>
2021-03-31 15:43:07 +0100

    Initial typescript configuration

---
+++ out/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+var world = "world";
+function hello(word) {
+    if (word === void 0) { word = world; }
+    return "Hello " + world + "! ";
+}
+exports.hello = hello;
+console.log(hello("world"));
+//# sourceMappingURL=index.js.map
